2efabe8fd00459a50a0fe829fa13adbd2f075461
sqysi <syquyy3110@gmail.com>
2025-12-05 16:08:30 +0700

    buoi 2

---
+++ Example02/Form1.Designer.cs
@@ -0,0 +1,55 @@
+﻿namespace Example02
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.Windows.Forms.Button bt_OK;
+        #region Windows Form Designer generated code
+        private void InitializeComponent()
+        {
+            bt_OK = new Button();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // bt_OK
+            // 
+            bt_OK.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            bt_OK.Location = new Point(631, 531);
+            bt_OK.Name = "bt_OK";
+            bt_OK.Size = new Size(80, 15);
+            bt_OK.TabIndex = 0;
+            bt_OK.Text = "OK";
+            bt_OK.Click += bt_OK_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(267, 320);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 54);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            ClientSize = new Size(701, 473);
+            Controls.Add(button1);
+            Controls.Add(bt_OK);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        private Button button1;
+        #endregion
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+
+
+    }
+}
---
+++ Example03/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Example03
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            tbYear = new TextBox();
+            textBox2 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(27, 65);
+            label1.Name = "label1";
+            label1.Size = new Size(44, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Year";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(27, 108);
+            label2.Name = "label2";
+            label2.Size = new Size(127, 25);
+            label2.TabIndex = 1;
+            label2.Text = "PhoneNumber";
+            // 
+            // tbYear
+            // 
+            tbYear.Location = new Point(172, 65);
+            tbYear.Name = "tbYear";
+            tbYear.Size = new Size(222, 31);
+            tbYear.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(172, 108);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(222, 31);
+            textBox2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(492, 207);
+            Controls.Add(textBox2);
+            Controls.Add(tbYear);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox tbYear;
+        private TextBox textBox2;
+    }
+}
